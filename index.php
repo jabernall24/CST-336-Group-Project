38b21db627c4860abce68f651a2a884e9dbff28e
Jesus Andres Bernal Lopez <jebernal@csumb.edu>
2019-04-19 02:54:31 +0000

    initial setup

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
-<html><body>
-<?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that is emitted to the Output tab of the console
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Home Page </title>
+    </head>
+    <body>
 
-echo 'Hello world from Cloud9!';
-
-?>
-</body>
+    </body>
 </html>
