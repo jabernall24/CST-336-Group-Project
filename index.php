f10e9343e7bb8b6530a2b83ffa764231657c9bfc
Jesus Andres Bernal Lopez <jebernal@csumb.edu>
2019-04-27 08:41:44 +0000

    redesigned the login page

--- index.php
+++ index.php
@@ -2,19 +2,36 @@
 <html>
     <head>
         <title> Home </title>
+        <!--jquery-->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        
+        <!--bootstrap-->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        
+        <!--css-->
         <link rel="stylesheet" href="css/index.css" type="text/css" />
     </head>
     
     <body>
-        <div id="homePageContainer">
-            <div id="homePageHeader">
-                <h1>WEBSITE NAME HERE</h1>
+        <nav class="navbar navbar-expand-lg">
+            <h1>WEBSITE NAME HERE</h1>
+            
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
             </div>
-        </div>
+        </nav>
         
+        <form action="login.php">
+            <button id="logInBtn" class="btn btn-primary">Log in/Sign up</button>
+        </form>
         <br/>
         
         <div id="search">
@@ -26,7 +43,7 @@
         
         <main>
             <div id="left">
-                <img src="" alt="" id="featuredCarImage">
+                <img src="" alt="featured car image" id="featuredCarImage">
             </div>
             <div id="right">
                 <strong>Price: </strong><span id="featuredCarPrice"></span> <br/>
@@ -34,9 +51,9 @@
                 <strong>Transmission: </strong><span id="featuredCarTransmission"></span> <br/>
                 <strong>Color: </strong><span id="featuredCarColor"></span> <br/>
                 
-                <a href="#" class="btn btn-info btn-lg">
+                <button href="#" class="btn btn-info btn-lg">
                     <span class="glyphicon glyphicon-shopping-cart"></span> Add to cart
-                </a>
+                </button>
                 
             </div>
         </main>
--- login.php
+++ login.php
@@ -13,27 +13,36 @@
 <html>
     <head>
         <title> Admin Login Screen </title>
-        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        
+        <link rel="stylesheet" href="css/login.css" type="text/css" />
     </head>
     
     <body>
-        <form method="POST" action="loginProcess.php">
-            Username: <input type="text" name="username" id="username"/> <br/>
-            
-            Password: <input type="password" name="password"/> <br/>
-            
-            <input type="submit" value="Login!" >
-            <br>
-            <br>
-            <br>
-            *Test Note*    Login using an account under your om_admin database                
-        </form>
-    <script>
+        <main>
+            <div id="websiteImage">
+                <img src="img/logo.jpg"></img>
+            </div>
+            <div id="loginSignUp">
+                    <form method="POST" action="loginProcess.php">
+                        <h1>Login</h1>
+                        Username: <br/>
+                        <input type="text" class="form-control form-rounded" name="username" id="username"> <br/>
+                        Password: <br/>
+                        <input type="password" class="form-control form-rounded" name="password" id="password"> <br/>
     
-    </script>
+                        <button type="submit" class="btn btn-outline-primary">Login</button>
+                        <button type="submit" class="btn btn-outline-primary">Sign up</button>
+                        <br>
+                        <br>
+                        <br>
+                        *Test Note*    Login using an account under your om_admin database                
+                    </form>
+            </div>
+        </main>
 
     </body>
 </html>
